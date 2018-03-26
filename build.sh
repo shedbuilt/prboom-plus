c120fd33bb314cc4060604219be7d96b508a9810
Auston Stewart <auston@shedbuilt.net>
2018-03-25 17:10:02 -1000

    Updating package metadata

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-./configure --disable-gl && \
-make -j $SHED_NUMJOBS && \
-make DESTDIR="$SHED_FAKEROOT" install-strip || exit 1
-install -dm755 "${SHED_FAKEROOT}/usr/local/bin"
-mv "${SHED_FAKEROOT}/usr/local/games/prboom-plus" "${SHED_FAKEROOT}/usr/local/bin"
-mv "${SHED_FAKEROOT}/usr/local/games/prboom-plus-game-server" "${SHED_FAKEROOT}/usr/local/bin"
+./configure --disable-gl &&
+make -j $SHED_NUMJOBS &&
+make DESTDIR="$SHED_FAKEROOT" install-strip &&
+install -dm755 "${SHED_FAKEROOT}/usr/local/bin" &&
+mv "${SHED_FAKEROOT}/usr/local/games/prboom-plus" "${SHED_FAKEROOT}/usr/local/bin" &&
+mv "${SHED_FAKEROOT}/usr/local/games/prboom-plus-game-server" "${SHED_FAKEROOT}/usr/local/bin" &&
 rmdir "${SHED_FAKEROOT}/usr/local/games"
