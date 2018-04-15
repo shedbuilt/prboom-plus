2561437f3eeab980143ff10ad3ef000633769ae0
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:14:30 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ./configure --disable-gl &&
-make -j $SHED_NUMJOBS &&
-make DESTDIR="$SHED_FAKEROOT" install-strip &&
-install -dm755 "${SHED_FAKEROOT}/usr/local/bin" &&
-mv "${SHED_FAKEROOT}/usr/local/games/prboom-plus" "${SHED_FAKEROOT}/usr/local/bin" &&
-mv "${SHED_FAKEROOT}/usr/local/games/prboom-plus-game-server" "${SHED_FAKEROOT}/usr/local/bin" &&
-rmdir "${SHED_FAKEROOT}/usr/local/games"
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install-strip &&
+install -dm755 "${SHED_FAKE_ROOT}/usr/local/bin" &&
+mv "${SHED_FAKE_ROOT}/usr/local/games/prboom-plus" "${SHED_FAKE_ROOT}/usr/local/bin" &&
+mv "${SHED_FAKE_ROOT}/usr/local/games/prboom-plus-game-server" "${SHED_FAKE_ROOT}/usr/local/bin" &&
+rmdir "${SHED_FAKE_ROOT}/usr/local/games"
